16ae920726109635b4f96f76a13ffa0fcec81b25
이대우 <lidy1112@gmail.com>
2024-02-27 05:05:49 +0900

    SwaggerConfig 삭제. Annotation 으로 변경

--- src/main/java/com/lidy/redis/RedisApplication.java
+++ src/main/java/com/lidy/redis/RedisApplication.java
@@ -3,6 +3,10 @@ package com.lidy.redis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "https://www.lidy.synology.me/redis"), @Server(url = "/")})
 @SpringBootApplication
 public class RedisApplication {
 
